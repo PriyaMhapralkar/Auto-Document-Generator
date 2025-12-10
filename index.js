c38debc8b67b5f8ace53c8fa10c7cb74739eabd3
PriyaMhapralkar <priyamhapralkar11@gmail.com>
2025-12-10 10:46:49 +0530

    Make Google OAuth callback configurable via GOOGLE_CALLBACK_URL

--- index.js
+++ index.js
@@ -269,6 +269,7 @@ app.get('/logout', (req, res) => {
   });
 });
 
-app.listen(port, () => {
-    console.log(`Server running on http://localhost:${port}`);
+
+app.listen(port, '0.0.0.0', () => {
+    console.log(`Server running on port ${port}`);
 });
